d89d17a074aa83cf6beb74158380c64b6c725a53
THe xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-09-01 15:55:12 +0530

    Create String Similarity.cpp

---
+++ String Similarity.cpp
@@ -0,0 +1,27 @@
+
+#include "bits/stdc++.h"
+ 
+#define gOdSpEeD ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(0)
+#define Int long long
+#define all(x) x.begin(), x.end()
+#define el '\n'
+#define ints unsigned
+#define cy cout << "YES" << end 
+#define cn cout <<"NO" << end
+#define elif else if
+#define test int t; cin>>t; while(t--)
+ 
+using namespace std;
+ 
+int main () {
+    gOdSpEeD;
+    test{
+        int x;cin>>x;
+        string s;cin>>s;
+        for(int i=0;i<x;i++)cout<<s[x-1]; 
+        cout<<endl;
+    }//test
+ 
+    return 0;
+}
+//Aug/25/2020 21:20
