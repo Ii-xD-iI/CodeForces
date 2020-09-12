931e7a22081f7a8d14c930801aea7016d3e89445
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-09-12 21:51:23 +0530

    smallest (even/odd) composite

---
+++ 800/472A - Design Tutorial: Learn from Math.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+//PRAY :DOLPHINGARLIC: orz orz orz orz orz 
+//PRAY :DORI: orz orz orz orz orz orz orz 
+//PRAY :SAHIL KUCHLOUS: orz orz orz orz orz
+#define GS ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(0)
+#define all(x) x.begin(), x.end()
+#define el '\n'
+#define Int long long
+#define elif else if
+#define test int t; cin>>t; while(t--)
+ 
+using namespace std;
+ 
+ 
+int main(){
+	GS;
+	int x;cin>>x;
+	if(x%2 == 0){
+		cout << 4 << " " << x-4 << endl;
+	}
+	else cout << 9 << " " << x-9 << endl;
+	return 0;
+}
+/*
+9:50 PM
+9/12/2020 mm/dd/yyyy
+*/
