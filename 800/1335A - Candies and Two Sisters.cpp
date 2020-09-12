b29bde1e3fab7fc6aef0230b56bc1abe06574266
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-09-13 00:42:46 +0530

    mafs yea struggling : /

---
+++ 800/1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+//PRAY :DOLPHINGARLIC: orz orz orz orz orz 
+//PRAY :DORI: orz orz orz orz orz orz orz 
+//PRAY :SAHIL KUCHLOUS: orz orz orz orz orz
+#define GS ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(0)
+#define all(x) x.begin(), x.end()
+#define el '\n'
+#define Int long long
+#define elif else if
+#define test int t; cin>>t; while(t--)
+
+using namespace std;
+
+
+int main(){
+	GS;
+    test{
+    	Int n; cin>>n; 
+    	cout << ((n-1)>>1) << endl;
+    }
+    return 0;
+}
+/*
+12:41 AM
+9/13/2020 mm/dd/yyyy
+*/
