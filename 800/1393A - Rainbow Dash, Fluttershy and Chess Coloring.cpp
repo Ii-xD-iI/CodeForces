bf8d23650d188f606943a4459b348eabebfa726d
The xD <70330230+Ii-xD-iI@users.noreply.github.com>
2020-09-29 00:18:36 +0530

    do harder problems lol

---
+++ 800/1393A - Rainbow Dash, Fluttershy and Chess Coloring.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+//PRAY :ORZ FENCING: orz orz orz orz orz 
+//PRAY :DOLPHINGARLIC: orz orz orz orz orz 
+//PRAY :DORI: orz orz orz orz orz orz orz 
+//PRAY :SAHIL KUCHLOUS: orz orz orz orz orz
+//pray :stefan: orz orz orz orz orz orz orz
+//pray :foshy: orz orz orz orz orz orz orz
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define elif else if
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(0);
+    
+    int t;cin>>t;
+    while(t--){int n; cin>>n;cout << (n>>1)+1 << endl;}
+    return 0;
+}
+//TASKKILL /F /IM main.exe
+/*
+11:59 PM
+9/28/2020 m/dd/yyyy
+*/
